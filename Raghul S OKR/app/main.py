fbc2018efa8b25192de3f3d1b3f18bd83e0d6832
Raghul-S-Optisol <129938074+Raghul-S-Optisol@users.noreply.github.com>
2024-10-20 20:25:14 +0530

    Update main.py

--- Raghul S OKR/app/main.py
+++ Raghul S OKR/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routers import user_router
+from app.routers import invoice_router
 
 app = FastAPI()
 
 app.include_router(user_router.router, prefix="/api", tags=["Users"])
+app.include_router(invoice_router.router, prefix="/invoice", tags=["Invoice"])
 
 @app.get("/")
 async def root():
